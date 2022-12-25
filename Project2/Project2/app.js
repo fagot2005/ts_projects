890a2458e59219a7121a78bba9c77f0d61f0c400
fagot2005 <v.lashchenko.1976@gmail.com>
2022-12-25 18:52:25 +0200

    Add new function

--- Project2/Project2/app.js
+++ Project2/Project2/app.js
@@ -1,6 +1,11 @@
 "use strict";
 let a = 10;
 let b = 20;
-console.log(a + b);
+let cobra = 5000;
+console.log(a + b + cobra);
 console.log('Hello world');
+function FullName(first, second) {
+    return first + ' ' + second;
+}
+console.log(FullName('Ivan', 'Ivanko'));
 //# sourceMappingURL=app.js.map
--- Project2/Project2/app.ts
+++ Project2/Project2/app.ts
@@ -3,3 +3,9 @@ let b: number = 20;
 let cobra: number = 5000;
 console.log(a + b + cobra);
 console.log('Hello world');
+
+function FullName(first: string, second: string): string {
+    return first+' '+ second;
+}
+
+console.log(FullName('Ivan', 'Ivanko'));
